97acbfadfbc887e1a0e98867b81d5182ffc8b6eb
Akhilesh datt <akhilesh.05.dutt@gmail.com>
2026-05-28 00:42:02 +0530

    next

--- hello.java
+++ hello.java
@@ -1,9 +1,6 @@
 class hello{
     public static void main(String args[]){
         System.out.print("helloo  buddy");
-
-        for(int i=0; i<10;i++){
-            System.out.println("hellooo");
-        }
+        
     }
 }
